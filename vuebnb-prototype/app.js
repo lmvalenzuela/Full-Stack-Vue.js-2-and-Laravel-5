ef76023936fe64f4e6ae47077a75a09f0cf93229
Luis M. Valenzuela <luis.valenzuela@ktc.com.mx>
2018-05-21 18:03:10 -0500

    Page 60

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,9 +1,25 @@
-
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'My apartment',
-        address: '12 My Street, My City, My Country',
-        about: 'This is a description of my apartment.'
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted: true,
+        modalOpen: false,
+    },
+    watch: {
+        modalOpen: function(){
+            var className = 'modal-open';
+            if (this.modalOpen){
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }
     }
 });
