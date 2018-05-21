1274725d9388550a38ac54bb709a5509b6cce940
Luis Miguel Valenzuela <luis.valenzuela@ktc.com.mx>
2018-05-21 12:24:37 -0500

    Page 32

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,9 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'My apartment',
+        address: '12 My Street, My City, My Country',
+        about: 'This is a description of my apartment.'
+    }
+});
